1f9f9f08a8d7d8a334963d7e952311fb50a0b391
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2023-07-31 23:01:04 +0200

    date picker strugle

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -1,21 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { ReactComponent as CalenderIcon } from "./assets/calender.svg";
 
-
 export default function DateForm({ placeholder }) {
-    const [value, setValue] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [isDatePickerOpen, setDatePickerOpen] = useState(false);
+  const datePickerRef = useRef(null);
 
-    
-    React.useEffect(() => {
-        setValue(placeholder);
-    }, [placeholder]);
+  const handleIconClick = () => {
+    setDatePickerOpen(true);
+  };
 
-    return (
-        <div className="date-picker-form">
-            <div className="date-text">{value}</div>
-            <div className="icon-help">
-                <CalenderIcon className="calender-icon"></CalenderIcon>
-            </div>
+  const handleDateChange = (event) => {
+   
+    const selectedDate = event.target.value;
+
+  
+    setStartDate(selectedDate);
+    setDatePickerOpen(false);
+  };
+
+  const handleFormClick = () => {
+    setDatePickerOpen(true);
+  };
+
+  
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (datePickerRef.current && !datePickerRef.current.contains(event.target)) {
+        setDatePickerOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [datePickerRef]);
+
+  const handlePlaceholder = () => {
+    return startDate ? startDate : placeholder;
+  };
+
+  return (
+    <div className="date-picker-form" onClick={handleFormClick}>
+      <div className="date-text" onClick={handleFormClick}>
+        {handlePlaceholder()}
+      </div>
+      {isDatePickerOpen && (
+        <div ref={datePickerRef}>
+          <input type="date" value={startDate || ""} onChange={handleDateChange} />
         </div>
-    );
+      )}
+      <div className="icon-help">
+        <CalenderIcon className="calender-icon" onClick={handleIconClick} />
+      </div>
+    </div>
+  );
 }
