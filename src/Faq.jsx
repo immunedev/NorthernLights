c770626a137ac4b0939ef98b5843be55065f8575
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2023-08-14 23:41:45 +0200

    add

---
+++ src/Faq.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './Header';
+
+export default function Faq(){
+    return(
+        <div className="static-page-container">
+            <Header></Header>
+            <div className="static-page-content">
+                
+            </div>
+
+        </div>
+    )
+}
