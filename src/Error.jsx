e40bd2c4e5c28cf311153bc81287db80e3e5b306
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2023-08-27 14:50:39 +0200

    small change

--- src/Error.jsx
+++ src/Error.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function ErrorPage(){
     return(
         <div className="error-page-container">
-            <div className="error-page-text">Error</div>
+            <div className="error-page-text">This is just a test site, you can not book a trip!</div>
             <div></div>
         </div>
     );
