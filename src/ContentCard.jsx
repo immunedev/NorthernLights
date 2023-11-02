cd5fe468f7b8f173771216a2a9abf8e5a4a275ec
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2023-11-02 23:24:21 +0100

    added api
    strapi api

--- src/ContentCard.jsx
+++ src/ContentCard.jsx
@@ -22,34 +22,34 @@ export default function ContentCard({ data, onTripSelect }) {
 
         <div className='card-content'>
 
-          <div className='card-title'>{data.title}</div>
+          <div className='card-title'>{data.Trip_Title}</div>
           <div className='card-details'>
             <div className='card-details-cell'>
               <FlightOut className='icon-32'></FlightOut>
-              <p className='details-text'>{data.departure}</p>
+              <p className='details-text'>{data.Flight_From}</p>
             </div>
             <div className='card-details-cell'>
               <DateIcon className='icon-32'></DateIcon>
-              <p className='details-text'>{data.length}</p>
+              <p className='details-text'>{data.How_long}</p>
             </div>
             <div className='card-details-cell'>
               <FlightIn className='icon-32'></FlightIn>
-              <p className='details-text'>{data.location}</p>
+              <p className='details-text'>{data.Flight_to}</p>
             </div>
             <div className='card-details-cell'>
               <Hotel className='icon-32'></Hotel>
-              <p className='details-text'>{data.hotel}</p>
+              <p className='details-text'>{data.Hotel}</p>
             </div>
           </div>
 
           <div className='card-desc'>
             <div className='card-long-desc'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              {data.Trip_Description}
             </div>
             <div className='card-price-cell'>
               <div className='card-price-text'>
-                <div className='price'>{data.price}</div>
-                <div className='price'>{data.child_price}</div>
+                <div className='price'>{data.Price_Adult}$</div>
+                <div className='price'>{data.Price_child}$</div>
                 <div className='sub-price'>Per adult</div>
                 <div className='sub-price'>Per child</div>
               </div>
--- src/ContentContainer.jsx
+++ src/ContentContainer.jsx
@@ -1,20 +1,44 @@
-import React, { useEffect, useRef } from 'react';
-import ContentCard from './ContentCard'
-import { tripData } from './data';
-
-
+import React, { useEffect, useState } from 'react';
+import ContentCard from './ContentCard';
 
 export default function ContentContainer({ onTripSelect }) {
+  const [apiData, setApiData] = useState({ data: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:1337/api/trips');
+
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+
+        const data = await response.json();
+
+        if (data.data && Array.isArray(data.data)) {
+          setApiData(data);
+        } else {
+          throw new Error('Invalid API response data');
+        }
+      } catch (error) {
+        console.error('Error fetching or processing data:', error);
+        setApiData({ data: [] }); 
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className='content-container'>
-        <div className="content-box" >
-          {tripData.trips.map((trip) => (
-            <ContentCard key={trip.id} data={trip} onTripSelect={onTripSelect}/>
-            
-          ))}
-        </div>
+      <div className="content-box" >
+        {apiData.data.map((trip) => (
+          <ContentCard key={trip.id} data={trip.attributes} onTripSelect={onTripSelect}/>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+
 
