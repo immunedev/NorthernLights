7b4c0737191537f57b9c42ff5393d1421979d7d5
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2023-11-26 20:02:53 +0100

    fix

--- src/TripPage.jsx
+++ src/TripPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
 import { useParams, Link  } from 'react-router-dom';
 import { ReactComponent as LocationIcon } from "./assets/location.svg";
 import { ReactComponent as DateIcon } from "./assets/date.svg";
